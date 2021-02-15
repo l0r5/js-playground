87f28755890f26e22f16d06294d45d224be24ca7
l0r5 <lars-braitinger@web.de>
2021-02-15 20:33:49 +0100

    Add some first project files

--- src/js/my-script.js
+++ src/js/my-script.js
@@ -1,1 +1,28 @@
-alert(42);
+"use strict";
+
+const a1 = [1, 2, 3, 4];
+
+console.log(a1[0]);
+console.log(a1[1]);
+
+
+// console.log("Sum: " + sum(1, 2));
+
+function sum(a, b) {
+    console.log(a + b);
+}
+
+sum(1, 1);
+
+document.getElementById("sum-button").onclick = function () {
+  sum(4, 6);
+};
+
+for(let i in navigator) {
+    console.log(i + ":" + navigator[i]);
+}
+
+for (let attr in navigator.geolocation) {
+    console.log(attr + ":" + navigator.geolocation[attr]);
+
+}
